837f3eaefb10593feee6b7207678af2f1681f409
AlexCeban13 <alexceban1998@gmail.com>
2022-11-22 18:09:57 -0500

    Fixed missing CSS for the admin page in production

--- src/src/settings.py
+++ src/src/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cf%(5k*go5f6+cf)lu2y4!wdcu(vpdmcz5axz_0%l1!)_na6g1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -91,6 +91,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'src.wsgi.application'
 
 
+STATIC_ROOT =  'static'
+
+
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -136,7 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 #Create static folder 
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
